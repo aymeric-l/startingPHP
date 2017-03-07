9835c1909e7a6b52afc1fffd9fbd463c03e24dca
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-07 14:01:24 +0100

    exo 3

--- variable.php
+++ variable.php
@@ -10,7 +10,14 @@
 $nom = 'lacoste';
 $prenom = 'aymeric';
 $age = 22;
+$killometre = 1;
 echo '<h2>Nom : '.$nom.'. Prenom : '.$prenom.'. Age : '.$age.'!</h2>';
+
+echo 'Killometre : '.$killometre.'</br>';
+$killometre = 3;
+echo 'Killometre : '.$killometre.'</br>';
+$killometre = 125;
+echo 'Killometre : '.$killometre.'</br>';
 ?>
 
 </body>
