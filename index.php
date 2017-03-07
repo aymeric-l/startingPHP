0820a317cd36a2af38702a8da6361dbbcdabba33
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-07 13:58:18 +0100

    exo1 et 2

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exo php !</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<a href="variable.php">Exercice 1</a>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<?php 
+$nom = 'lacoste';
+$prenom = 'aymeric';
+$age = 22;
+echo '<h2>Nom : '.$nom.'. Prenom : '.$prenom.'. Age : '.$age.'!</h2>';
+?>
+
+</body>
+</html>
