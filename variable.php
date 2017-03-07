214d632223f5d96d9711fe42dd17cb42d23ba984
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-07 14:13:47 +0100

    exo 4

--- variable.php
+++ variable.php
@@ -11,14 +11,23 @@ $nom = 'lacoste';
 $prenom = 'aymeric';
 $age = 22;
 $killometre = 1;
-echo '<h2>Nom : '.$nom.'. Prenom : '.$prenom.'. Age : '.$age.'!</h2>';
+$leBool = true;
+$leString = 'Chaine';
+$leInt = 666;
+$leFloat = 3.3;
+
+echo '<p id="test">Nom : '.$nom.'. Prenom : '.$prenom.'. Age : '.$age.'!</p>';
 
 echo 'Killometre : '.$killometre.'</br>';
 $killometre = 3;
 echo 'Killometre : '.$killometre.'</br>';
 $killometre = 125;
 echo 'Killometre : '.$killometre.'</br>';
+
+echo 'Le boolean est : '.$leBool.'.</br>Le string est : '.$leString.'.</br>Le INT est : '.$leInt.'.</br>Et enfin le float est : '.$leFloat.'.</br>';
+
 ?>
+
 
 </body>
 </html>
