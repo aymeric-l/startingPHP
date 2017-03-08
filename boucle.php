5b573ed90dff696768df3372a3eaf1f2ac65357f
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-08 09:58:00 +0100

    BOUCLE 2

--- boucle.php
+++ boucle.php
@@ -7,11 +7,19 @@
 <body>
 <?php 
 $boucleDix = 0;
+$partieDeux = 0;
+$partieDeuxChoix = 3;
+
 while($boucleDix != 10){
 	echo $boucleDix;
 	$boucleDix++;
 }
+echo '</br>';
 
+while($partieDeux <= 20){
+	echo $partieDeux*$partieDeuxChoix.' ';
+	$partieDeux++;
+}
 
 ?>
 </body>
