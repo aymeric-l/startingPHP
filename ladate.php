bf29e4dbfafc51b3aa902d17bdda63713f28a67b
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-09 16:08:06 +0100

    date 3

--- ladate.php
+++ ladate.php
@@ -1,4 +1,6 @@
 <?php 
 	$laDate = strftime('%A %d %B %Y, %H:%M:%S');
 	echo '<h1 style ="color:red; font-size:75px">'.$laDate.'</h1>';
+	echo 'Timestamp du jour : '. time().'</br>';
+	echo 'Timestamp du mardi 2 mars 2016, 15h34m12s : '. mktime(15, 34, 12, 3, 2, 2016);
 ?>
