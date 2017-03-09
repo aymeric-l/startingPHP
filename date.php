9cef2456900ea94ad2e753ac670be61a29c3a851
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-09 15:50:38 +0100

    date 2

--- date.php
+++ date.php
@@ -1,4 +1,25 @@
-<?php 
-	setlocale(LC_TIME, 'fra_fra');
-	echo strftime('%A %d %B %Y, %H:%M');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="jquery-3.1.1.min.js"></script>
+	<script type="text/javascript">
+		function date() {
+		   $.ajax({
+		      type: "GET",
+		      url: "ladate.php"
+		   }).done(function (html) {
+		      $('#oui').html(html); // Retourne dans #maDiv le contenu de ta page
+		      setTimeout(date, 1000);
+		   });
+		}      
+		date();
+	</script>
+</head>
+<body>
+<div id="oui">
+
+</div>
+
+</body>
+</html>
---
+++ ladate.php
@@ -0,0 +1,4 @@
+<?php 
+	$laDate = strftime('%A %d %B %Y, %H:%M:%S');
+	echo '<h1 style ="color:red; font-size:75px">'.$laDate.'</h1>';
+?>
